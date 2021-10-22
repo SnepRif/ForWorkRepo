0623e515bc6e310555fc006a1409cb292e719d58
OlegUvarovskiy <Riff96@yandex.ru>
2021-10-22 12:30:18 +0400

    OneToManyProj
    one more mvc proj for one to many database relationship

---
+++ FootballClubProj/FootballClubProj/Controllers/HomeController.cs
@@ -0,0 +1,90 @@
+﻿using FootballClubProj.Models;
+using System.Web.Mvc;
+using System.Data.Entity;
+using System.Linq;
+
+namespace FootballClubProj.Controllers
+{
+    public class HomeController : Controller
+    {
+        SoccerContext db = new SoccerContext();
+
+        public ActionResult Index()
+        {
+            var players = db.Players.Include(p => p.Team);
+            return View(players.ToList());
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            // Список команд для передачи в представление
+            SelectList teams = new SelectList(db.Teams, "Id", "Name");
+
+            ViewBag.Teams = teams;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Player player)
+        {
+            db.Players.Add(player);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+            var player = db.Players.Find(id);
+            if (player != null)
+            {
+                var teams = new SelectList(db.Teams, "Id", "Name", player.TeamId);
+                ViewBag.Teams = teams;
+                return View(player);
+            }
+            return RedirectToAction("Index");
+
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Player player)
+        {
+            db.Entry(player).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return HttpNotFound();
+            }
+            var player = db.Players.Find(id);
+            if (player != null)
+            {
+                db.Players.Remove(player);
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
