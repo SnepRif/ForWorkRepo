23f1ad99b7f22b333476d830af134ed07e9ea147
OlegUvarovskiy <Riff96@yandex.ru>
2021-11-10 10:28:47 +0400

    data filter

--- FootballClubProj/FootballClubProj/Controllers/HomeController.cs
+++ FootballClubProj/FootballClubProj/Controllers/HomeController.cs
@@ -2,6 +2,8 @@
 using System.Web.Mvc;
 using System.Data.Entity;
 using System.Linq;
+using System;
+using System.Collections.Generic;
 
 namespace FootballClubProj.Controllers
 {
@@ -9,10 +11,39 @@ namespace FootballClubProj.Controllers
     {
         SoccerContext db = new SoccerContext();
 
-        public ActionResult Index()
+        public ActionResult Index(int? team, string position)
         {
-            var players = db.Players.Include(p => p.Team);
-            return View(players.ToList());
+            IQueryable<Player> players = db.Players.Include(p => p.Team);
+            if (team != null && team != 0)
+            {
+                players = players.Where(p => p.TeamId == team);
+            }
+            if (!String.IsNullOrEmpty(position) && !position.Equals("Все"))
+            {
+                players = players.Where(p => p.Position == position);
+            }
+
+            List<Team> teams = db.Teams.ToList();
+            //устанавливаем начальный элемент который позволяет выбрать всех
+            teams.Insert(0, new Team { Name = "Все", Id = 0 });
+
+            PlayerListViewModel plvm = new PlayerListViewModel
+            {
+                Players = players.ToList(),
+                Teams = new SelectList(teams, "Id", "Name"),
+                Positions = new SelectList(new List<string>()
+                {
+                    "Все",
+                    "Нападающий",
+                    "Полузащитник",
+                    "Защитник",
+                    "Вратарь"
+
+                })
+            };
+            return View(plvm);
+
+
         }
 
         [HttpGet]
---
+++ FootballClubProj/FootballClubProj/Models/PlayerListViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FootballClubProj.Models
+{
+    public class PlayerListViewModel
+    {
+        /// <summary>
+        /// Хранит отсортированных игроков
+        /// </summary>
+        public IEnumerable<Player> Players { get; set; }
+
+        /// <summary>
+        /// Критерий сортировки
+        /// </summary>
+        public SelectList Teams { get; set; }
+
+        /// <summary>
+        /// Критерий сортировки
+        /// </summary>
+        public SelectList Positions { get; set; }
+
+
+    }
+}
