b9634989177b326e437865c3b9e867605e8c5194
OlegUvarovskiy <Riff96@yandex.ru>
2021-11-10 19:01:15 +0400

    attributes
    add some model attributes

--- ClothingStoreApp/ClothingStoreApp/Models/Clothes.cs
+++ ClothingStoreApp/ClothingStoreApp/Models/Clothes.cs
@@ -1,17 +1,26 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
 
 namespace ClothingStoreApp.Models
 {
     public class Clothes
     {
+        [HiddenInput(DisplayValue = false)]
         public int ClothesId { get; set; }
+
+        [Display(Name = "Бренд")]
         public string BrandName { get; set; }
+
+        [Display(Name = "Наименование")]
         public string KindOf { get; set; }
+
+        [Display(Name = "Размер")]
         public string Size { get; set; }
+
+        [Display(Name = "Цена")]
         public decimal Price { get; set; }
 
-       
+
 
     }
 }
